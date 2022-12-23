b7c13a6fdb8b15c514535c3e529a33db9eb35699
Milan Hauth <milahu@gmail.com>
2022-12-23 18:07:43 +0100

    popup

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -5,20 +5,8 @@ import styles from "./Popup.module.css";
 const Popup = () => {
   return (
     <div class={styles.Popup}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p class="font-bold">
-          Edit <code>src/pages/popup/Popup.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+      <input/>
+      <button>Search</button>
     </div>
   );
 };
