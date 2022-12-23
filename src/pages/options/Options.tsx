226202c9d875abe273f1501eaf86d94c910c8a03
Milan Hauth <milahu@gmail.com>
2022-12-23 19:18:34 +0100

    options

--- src/pages/options/Options.tsx
+++ src/pages/options/Options.tsx
@@ -1,24 +1,42 @@
 import logo from "@assets/img/logo.svg";
 import "@src/styles/index.css";
 import styles from "./Options.module.css";
+import {createSignal} from "solid-js"
+import {For} from "solid-js"
 
 const Options = () => {
+  let searchButton;
+  const [getResults, setResults] = createSignal([
+    "result0",
+    "result0",
+  ])
+  function startSearch(event) {
+    event.preventDefault()
+    //console.log("submit")
+    searchButton.innerHTML = "Searching...";
+    searchButton.disabled = true;
+    // clear results
+    //console.log("clear")
+    setResults([])
+    // update results
+    //console.log("update")
+    setResults((old) => old.concat(["result1"]))
+    setResults((old) => old.concat(["result2"]))
+  }
   return (
     <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p class="font-bold">
-          Edit <code>src/pages/options/Options.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+      <form class={styles.form} onSubmit={startSearch}>
+        <input/>
+        <button ref={searchButton}>Search</button>
+      </form>
+      <ol class={styles.results}>
+        <For each={getResults()}>
+          {result => (
+            <li>result: {JSON.stringify(result, null, 2)}</li>
+          )}
+        </For>
+      </ol>
+      <pre>{JSON.stringify(getResults(), null, 2)}</pre>
     </div>
   );
 };
