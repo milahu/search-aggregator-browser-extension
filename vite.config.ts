cb7c6cfeb3062858e3762a2d199f47fda424c3a0
Milan Hauth <milahu@gmail.com>
2022-12-25 09:53:52 +0100

    try to make vite faster, no luck

--- vite.config.ts
+++ vite.config.ts
@@ -12,8 +12,18 @@ const outDir = resolve(__dirname, "dist");
 const publicDir = resolve(__dirname, "public");
 
 const isDev = process.env.__DEV__ === "true";
+//console.log("vite.config.ts: isDev", isDev)
 
-export default defineConfig({
+// TODO port
+
+export default defineConfig(({ command }) => ({
+  //base: command === 'serve' ? `http://localhost:${port}/` : '/dist/',
+  server: {
+    //port,
+    hmr: {
+      host: 'localhost',
+    },
+  },
   clearScreen: false,
   plugins: [solidPlugin(), crx({ manifest }), WindiCSS()],
   resolve: {
@@ -26,30 +36,48 @@ export default defineConfig({
   publicDir,
   build: {
     outDir,
-    sourcemap: isDev,
+    emptyOutDir: false,
+    //sourcemap: isDev,
+    sourcemap: isDev ? 'inline' : false,
+    minify: !isDev,
     rollupOptions: {
-      // input: {
+      input: {
       //   devtools: resolve(pagesDir, "devtools", "index.html"),
       //   panel: resolve(pagesDir, "panel", "index.html"),
       //   content: resolve(pagesDir, "content", "index.ts"),
       //   background: resolve(pagesDir, "background", "index.ts"),
       //   contentStyle: resolve(pagesDir, "content", "style.scss"),
-      //   popup: resolve(pagesDir, "popup", "index.html"),
+        popup: resolve(pagesDir, "popup", "index.html"),
       //   newtab: resolve(pagesDir, "newtab", "index.html"),
-      //   options: resolve(pagesDir, "options", "index.html"),
-      // },
-      // output: {
-      //   entryFileNames: "src/pages/[name]/index.js",
-      //   chunkFileNames: isDev
-      //     ? "assets/js/[name].js"
-      //     : "assets/js/[name].[hash].js",
-      //   assetFileNames: (assetInfo) => {
-      //     const { dir, name: _name } = path.parse(assetInfo.name);
-      //     // const assetFolder = getLastElement(dir.split("/"));
-      //     // const name = assetFolder + firstUpperCase(_name);
-      //     return `assets/[ext]/${name}.chunk.[ext]`;
-      //   },
-      // },
+        options: resolve(pagesDir, "options", "index.html"),
+      },
+      /*
+      output: {
+        entryFileNames: "src/pages/[name]/index.js",
+        chunkFileNames: isDev
+          ? "assets/js/[name].js"
+          : "assets/js/[name].[hash].js",
+        assetFileNames: (assetInfo) => {
+          const { dir, name: _name } = path.parse(assetInfo.name);
+          // const assetFolder = getLastElement(dir.split("/"));
+          // const name = assetFolder + firstUpperCase(_name);
+          return `assets/[ext]/${name}.chunk.[ext]`;
+        },
+      },
+      */
     },
   },
-});
+  optimizeDeps: {
+    include: [
+      //'vue',
+      //'@vueuse/core',
+      //'webextension-polyfill',
+    ],
+    exclude: [
+      //'@engine262/engine262',
+      //'sval',
+      'sval',
+      'acorn',
+    ],
+  },
+}));
