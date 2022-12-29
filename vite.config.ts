c68f23ddf81822b714d751f150a6f87e540007fa
Milan Hauth <milahu@gmail.com>
2022-12-29 15:38:31 +0100

    make dev build faster

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,75 @@
+/*
+TODO faster
+https://vitejs.dev/config/dep-optimization-options.html
+
+
+
+node_modules/vite/dist/node/chunks/dep-0bae2027.js
+
+async function doBuild(inlineConfig = {}) {
+
+    const rollupOptions = {
+        context: 'globalThis',
+        preserveEntrySignatures: ssr
+            ? 'allow-extension'
+            : libOptions
+                ? 'strict'
+                : false,
+        ...options.rollupOptions,
+        input,
+        plugins,
+        external,
+        onwarn(warning, warn) {
+            onRollupWarning(warning, warn, config);
+        },
+    };
+
+!!!!
+        // watch file changes with rollup
+        if (config.build.watch) {
+            const { watch } = await import('rollup');
+            const watcher = watch({
+                ...rollupOptions,
+                output: normalizedOutputs,
+                watch: {
+                    ...config.build.watch,
+                    chokidar: resolvedChokidarOptions,
+                },
+            });
+            return watcher;
+        }
+
+        // write or generate files with rollup
+        const { rollup } = await import('rollup');
+        const bundle = await rollup(rollupOptions);
+
+
+
+https://rollupjs.org/guide/en/#cache
+
+const rollup = require('rollup');
+let cache;
+
+async function buildWithCache() {
+  const bundle = await rollup.rollup({
+    cache // is ignored if falsy
+    // ... other input options
+  });
+  cache = bundle.cache; // store the cache object of the previous build
+  return bundle;
+}
+
+buildWithCache()
+  .then(bundle => {
+    // ... do something with the bundle
+  })
+  .then(() => buildWithCache()) // will use the cache of the previous build
+  .then(bundle => {
+    // ... do something with the bundle
+  });
+
+*/
+
 import { crx } from "@crxjs/vite-plugin";
 import { resolve } from "path";
 import fs from "fs";
@@ -23,6 +95,42 @@ const outputDefaults = {
   assetFileNames: `${outputAssetsDir}[name].[ext]`,
 }
 
+function traceBuild() {
+  return {
+    name: "trace-build",
+    transformIndexHtml() { console.log("trace-build: transformIndexHtml"); },
+    generateBundle() { console.log("trace-build: generateBundle"); }, // not called
+    //buildEnd() { console.log("trace-build: buildEnd: this", this); },
+    closeBundle() {
+      // If a plugin wants to retain resources across builds in watch mode,
+      // they can check for this.meta.watchMode in this hook
+      // and perform the necessary cleanup for watch mode in closeWatcher.
+      console.log("trace-build: closeBundle: this", this);
+      console.log("trace-build: closeBundle: this.cache", this.cache);
+      console.log("trace-build: closeBundle: this.cache.has", this.cache.has("node_modules/.pnpm/solid-bootstrap@1.0.13_solid-js@1.6.6/node_modules/solid-bootstrap/dist/esm/Alert.js"));
+      // TODO this.cache
+      //console.log("trace-build: closeBundle: this.moduleIds()", this.moduleIds())
+      //console.log("trace-build: writeBundle: bundle.cache", bundle.cache)
+      //rollupCache = bundle.cache // write cache
+      console.log("trace-build: closeBundle: time", new Date().toLocaleString("af"));
+    },
+    // bundle: Record<string, Chunk>
+    // no bundle.cache ...
+    writeBundle(_options, bundle) { console.log("trace-build: Object.keys(bundle)", Object.keys(bundle).slice(0, 10)); },
+    //resolveId() { console.log("trace-build: resolveId"); },
+    //load() { console.log("trace-build: load"); },
+    //transform() { console.log("trace-build: transform"); },
+    enforce: 'post',
+    apply: 'build',
+    /*
+    writeBundle(_options, bundle) {
+      console.log("writeBundle: bundle.cache", bundle.cache)
+      rollupCache = bundle.cache // write cache
+    },
+    */
+  }
+}
+
 export default defineConfig(({ command }) => {
 
 //console.log("vite.config.ts: command", command)
@@ -32,6 +140,21 @@ const isDev = (
 );
 //console.log("vite.config.ts: isDev", isDev)
 const isProd = !isDev;
+
+// based on node_modules/rollup/dist/es/rollup.js
+// use '+' as escape char to fix web extensions.
+// chrome error: Filenames starting with "_" are reserved for use by the system.
+const INVALID_CHAR_REGEX = /[\u0000-\u001F"#$&*+,:;<=>?[\]^`{|}\u007F_]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
+function sanitizeFileName(name) {
+  const match = DRIVE_LETTER_REGEX.exec(name);
+  const driveLetter = match ? match[0] : '';
+  const name2 = driveLetter + name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '+');
+  console.log(`sanitizeFileName: name2`, name2)
+  return name2
+}
+
+let rollupCache
 
 return {
   //base: command === 'serve' ? `http://localhost:${port}/` : '/dist/',
@@ -46,6 +169,7 @@ return {
     solidPlugin(),
     crx({ manifest }),
     WindiCSS(),
+    traceBuild(),
   ],
   resolve: {
     alias: {
@@ -56,6 +180,11 @@ return {
   },
   publicDir,
   build: {
+    //config.build.watch
+    //watch: true, // passed via CLI
+    watch: {
+      // https://rollupjs.org/guide/en/#watch-options
+    },
     outDir,
     //emptyOutDir: false, // keep old files. this is no caching
     // true -> 15 seconds
@@ -65,6 +194,7 @@ return {
     // false -> 15 seconds
     minify: isProd,
     rollupOptions: {
+      cache: rollupCache, // read cache
       // true -> 20 seconds
       // false -> 15 seconds
       treeshake: isProd,
@@ -79,6 +209,7 @@ return {
         preserveModules: isDev,
         preserveModulesRoot: __dirname,
         //manualChunks,
+        sanitizeFileName,
       },
       external: [
         //"sval",
@@ -92,12 +223,12 @@ return {
       //'webextension-polyfill',
     ],
     exclude: [
-      /*
-      'solid-js',
-      'sval',
+      //'solid-js', // The entry point "solid-js" cannot be marked as external
+      'sval', // SyntaxError: The requested module 'sval.js' does not provide an export named 'default'
       'acorn',
       "@hope-ui/solid",
-      */
+      "bootstrap",
+      "solid-bootstrap",
     ],
   },
 };
